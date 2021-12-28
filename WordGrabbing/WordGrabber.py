d3a31141472457a077d28cedec89d88195bee14a
Julius Tischbein <ju.tischbein@gmail.com>
2021-12-28 16:08:21 +0100

    Progress von WordGrabbing, not finished
    TODO: Language

--- WordGrabbing/WordGrabber.py
+++ WordGrabbing/WordGrabber.py
@@ -10,35 +10,6 @@ endpoint = SPARQL("https://query.wikidata.org/sparql")
 db = Database('./dbcfg.ini').connect()
 
 
-# Print iterations progress (stolen from stack overflow)
-def progressBar(iterable, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iterable    - Required  : iterable object (Iterable)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    total = len(iterable)
-    # Progress Bar Printing Function
-    def printProgressBar (iteration):
-        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-        filledLength = int(length * iteration // total)
-        bar = fill * filledLength + '-' * (length - filledLength)
-        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-    # Initial Call
-    printProgressBar(0)
-    # Update Progress Bar
-    for i, item in enumerate(iterable):
-        yield item
-        printProgressBar(i + 1)
-    # Print New Line on Complete
-    print(f'\r', end = printEnd)
-
 
 def save_error(link, word=""):
     f = open("errorlinks.txt", "a")
@@ -49,18 +20,7 @@ def save_error(link, word=""):
     f.write("\n")
     f.close()
 
-def test_josha(x, y):
-    for i in range(x):
-        y += x
-        print("penis")
-    return y
-
-
-def check_for_substances(nouns):
-    nouns = [noun.replace("\n", " ").strip() for noun in nouns]
-    # building the query string
-    values = [' '.join([f'"{noun}"@en' for noun in nouns[n * 50:n * 50 + 50]]) for n in range((len(nouns) // 50) + 1)]
-    queryValueFormatTags = '%s\n' * (len(values))
+def query(words):
     substanceQueryStr = """
     SELECT DISTINCT ?entry ?entry2
     WHERE { 
@@ -74,19 +34,25 @@ def check_for_substances(nouns):
       FILTER(lang(?entryLabel) = "en").
     }
     """
-    substanceQueryStr = substanceQueryStr % ("\n".join(values))
-    # executing the query
-    substanceQuery = Query(query=substanceQueryStr,
-                           name="Companies",
-                           lang="sparql")
-    substanceQueryResLoD = endpoint.queryAsListOfDicts(substanceQuery.query)
+
+    q = substanceQueryStr % words
+    query = Query(query=q,
+                            name="Recognized chemical compounds",
+                            lang="sparql")
+    queryResLoD = endpoint.queryAsListOfDicts(query.query)
+    entries = [record.get('entry') for record in queryResLoD]
+
+    return entries
+
+def check_for_any(nouns):
+    nouns = [noun.replace("\n", " ").strip() for noun in nouns]
+    # building the query string
+    values = [' '.join([f'"{noun}"@en' for noun in nouns[n * 50:n * 50 + 50]]) for n in range((len(nouns) // 50) + 1)]
+    
+    return query("\n".join(values))
 
 
-    substances = [record.get('entry') for record in substanceQueryResLoD]
-    return substances
-
-
-
+# TODO: Sprache berücksichtigen
 
 def process_article(line):
     link = line[0]
@@ -102,7 +68,7 @@ def process_article(line):
     nl = "\n"
 
     try:
-        s = check_for_substances([chunk.text for chunk in doc.noun_chunks])
+        s = check_for_any([chunk.text for chunk in doc.noun_chunks])
         if len(s) == 0:
             return
     except Exception as e:
@@ -110,28 +76,11 @@ def process_article(line):
 
     nouns = [f'"{chunk.text.replace(nl, "").strip()}"@en' for chunk in doc.noun_chunks]
 
-    substanceQueryStr = """
-    SELECT DISTINCT ?entry ?entry2
-    WHERE { 
-      VALUES ?entryLabel {%s}
-      {?entry wdt:P31 wd:Q83405.} UNION
-      {?sup wdt:P279 wd:Q83405.
-      ?entry wdt:P31 ?sup.}
-      ?entry rdfs:label|skos:altLabel ?entryLabel.
-      OPTIONAL {?entry wdt:P1542 ?entry2.
-      ?entry2 wdt:P31 wd:Q58734.}
-      FILTER(lang(?entryLabel) = "en").
-    }
-    """
-    print()
-    for word in progressBar(nouns, prefix = 'Progress:', suffix = 'Complete', length = 50):
+    
+    
+    for word in nouns:
         try:
-            q = substanceQueryStr % word
-            query = Query(query=q,
-                                   name="Recognized chemical compounds",
-                                   lang="sparql")
-            queryResLoD = endpoint.queryAsListOfDicts(query.query)
-            entries = [record.get('entry') for record in queryResLoD]
+            entries = query(word)
 
             if len(entries) > 0:
                 x = [urlparse(sub).path.split("/") for sub in entries]
@@ -142,7 +91,6 @@ def process_article(line):
         except Exception as e:
             #print("Unexpected error:", e)
             continue
-    print("\033[1A\033[1A")
 
 
 if db is None:
