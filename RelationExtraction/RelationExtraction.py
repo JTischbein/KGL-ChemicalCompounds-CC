d050bba7303a8b2e15a2bb4e40edb6cafb3f3a8e
Julius Tischbein <ju.tischbein@gmail.com>
2022-03-14 14:33:37 +0100

    Jtezt richtige RelationExtraction und config

--- RelationExtraction/RelationExtraction.py
+++ RelationExtraction/RelationExtraction.py
@@ -2,13 +2,18 @@ import spacy
 from nltk import tokenize
 import psycopg2
 from tqdm import tqdm
+from configparser import ConfigParser
 
 
-import config
+config = ConfigParser()
+config.read("../config.ini")
+HOST = config["POSTGRES"]["HOST"]
+PORT = config["POSTGRES"]["PORT"]
+DBNAME = config["POSTGRES"]["DBNAME"]
+USER = config["POSTGRES"]["USER"]
+PW = config["POSTGRES"]["PASSWORD"]
 
-nlp = spacy.load("de_core_news_sm")
-
-conn = psycopg2.connect(host=config.host, port=config.port, dbname=config.dbname, user=config.user, password=config.password)
+conn = psycopg2.connect(host=HOST, port=PORT, dbname=DBNAME, user=USER, password=PW)
 cur = conn.cursor()                                                                                                                                                   
 
 # We need the content of all articles that were mentioned in the companies table
@@ -27,6 +32,15 @@ chemicals = [chemical[0] for chemical in chemicals]
 cur.execute("SELECT DISTINCT synonym FROM locations")                                            
 locations = cur.fetchall()
 locations = [location[0] for location in locations]
+
+cur.execute("SELECT ch.chemical_formula, ch.tag, array_agg(DISTINCT ch2.synonym) FROM chemicals ch LEFT JOIN chemicals ch2 ON ch.chemical_formula = ch2.chemical_formula GROUP BY ch.chemical_formula, ch.tag")
+chemical_table = cur.fetchall()
+chemical_formulas = [formula[0] for formula in chemical_table]
+chemical_synonyms = [synonym[2] for synonym in chemical_table]
+chemical_dictionary = {}
+for synonyms, formula in zip(chemical_synonyms, chemical_formulas):
+    for synonym in synonyms:
+        chemical_dictionary[synonym] = formula
 
 # creates a noun list for each sentence
 def get_nouns(sentences):
@@ -48,14 +62,13 @@ def get_entities(noun_sentences):
             elif noun in chemicals:
                 chemical_list.append((noun, idx))
             elif noun in locations:
-               location_list.append((noun, idx))
+                location_list.append((noun, idx))
     return company_list, chemical_list, location_list 
 
 def get_chemical(article_chemicals, sentence_index):
     for chemical in article_chemicals:
         if chemical[1] == sentence_index:
             return chemical[0]
-
 
 def remove_chemical(article_chemicals, sentence_index):
     for chemical in article_chemicals:
@@ -84,7 +97,7 @@ def closest_entity(article_companies, article_chemicals, article_locations):
             if abs(company[1] - chemical[1]) < chemical_distance:
                 closest_chemical = chemical
                 chemical_distance = abs(company[1] - chemical[1])
-        cur.execute("INSERT INTO company_chemical (company, chemical, hierarchy, word_gap, article) VALUES (%s, %s, %s, %s, %s)", (company[0], closest_chemical[0], 3, chemical_distance, article[1]))
+        cur.execute("INSERT INTO company_chemical (company, chemical, hierarchy, word_gap, article) VALUES (%s, %s, %s, %s, %s)", (company[0], chemical_dictionary[closest_chemical[0]], 3, chemical_distance, article[1]))
         article_chemicals.remove(closest_chemical)
     # Then we have to find the closest location to the company
     for company in article_companies:
@@ -103,12 +116,10 @@ def closest_entity(article_companies, article_chemicals, article_locations):
 english_deps = ["nsubj", "nsubj_pass", "dobj", "iobj", "pobj"]
 german_deps = ["sb", "sbp", "oa", "og", "op"]
 
-article_count = 0
-
 english_nlp = spacy.load("en_core_web_sm")
 german_nlp = spacy.load("de_core_news_sm")
 
-for article in tqdm(articles):
+for article_count, article in enumerate(tqdm(articles)):
     article_count += 1
 
     if article[2] == "en":
@@ -124,9 +135,9 @@ for article in tqdm(articles):
     article_companies, article_chemicals, article_locations = get_entities(noun_sentences)
 
     chemical_indices = [chemical[1] for chemical in article_chemicals]
-    chemical_compounds = [chemical[0] for chemical in article_chemicals]
+    article_compounds = [chemical[0] for chemical in article_chemicals]
     location_indices = [location[1] for location in article_locations]
-    locations = [location[0] for location in article_locations]
+    article_countries = [location[0] for location in article_locations]
 
     for company in article_companies:
         sentence_index = company[1]
@@ -135,12 +146,12 @@ for article in tqdm(articles):
             class_1 = False
             for token in nlp(sentence):
                 # class 1: chemical is object of company or vice versa
-                if token.dep_ in deps and token.text in chemical_compounds:
-                    cur.execute("INSERT INTO company_chemical (company, chemical, hierarchy, word_gap, article) VALUES (%s, %s, %s, %s, %s)", (company[0], token.text, 1, 0, article[1]))
+                if token.dep_ in deps and token.text in article_compounds:
+                    cur.execute("INSERT INTO company_chemical (company, chemical, hierarchy, word_gap, article) VALUES (%s, %s, %s, %s, %s)", (company[0], chemical_dictionary[token.text], 1, 0, article[1]))
                     class_1 = True
             # class 2: no specific relationship, but appear in the same sentence
             if not class_1:
-                cur.execute("INSERT INTO company_chemical (company, chemical, hierarchy, word_gap, article) VALUES (%s, %s, %s, %s, %s)", (company[0], get_chemical(article_chemicals, sentence_index), 2,  0,  article[1]))
+                cur.execute("INSERT INTO company_chemical (company, chemical, hierarchy, word_gap, article) VALUES (%s, %s, %s, %s, %s)", (company[0], chemical_dictionary[get_chemical(article_chemicals)], sentence_index), 2,  0,  article[1])
             remove_chemical(article_chemicals, sentence_index)
 
         if sentence_index in location_indices:
@@ -148,7 +159,7 @@ for article in tqdm(articles):
             class_1 = False
             for token in nlp(sentence):
                 # class 1: location is object of company or vice versa
-                if token.dep_ in deps and token.text in locations:
+                if token.dep_ in deps and token.text in article_countries:
                     cur.execute("INSERT INTO company_location (company, location, hierarchy, word_gap, article) VALUES (%s, %s, %s, %s,  %s)", (company[0], token.text, 1, 0, article[1]))
                     class_1 = True
             # class 2: no specific relationship, but appear in the same sentence
@@ -165,8 +176,3 @@ conn.commit()
 
 cur.close()
 conn.close()
-
-
-# scispacy
-# neo4j
-# openai api
