d525f9a7d8556e9250be9c9a5bb5ad8693e388c1
Julius Tischbein <ju.tischbein@gmail.com>
2022-03-14 21:19:30 +0100

    ChemicalFormula Table Changed

--- RelationExtraction/ChemicalFormula.py
+++ RelationExtraction/ChemicalFormula.py
@@ -1,12 +1,11 @@
 import sys
-
 sys.path.append("../")
 
 from Database import Database
 
-db = Database("../config.ini").connect
+db = Database("../config.ini").connect()
 
-db_table = "chemical_location_relations"
+db_table = "company_chemical_relations"
 
 if db == None:
     print("Connection failure")
@@ -16,6 +15,6 @@ def line(l):
     synonym = l[0]
     formula = l[1]
 
-    db.execute('UPDATE ' + db_table + ' SET chemical_formula = %s WHERE synonym = %s', attributes=(formula, synonym))
+    db.execute('UPDATE ' + db_table + ' SET chemical_formula = %s WHERE chemical = %s', attributes=(formula, synonym))
 
 db.execute_and_run('SELECT DISTINCT synonym, chemical_formula FROM chemicals', callback=line, progress_bar=True)
