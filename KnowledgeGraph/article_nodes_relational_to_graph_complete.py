3315a0c38a2dc8ba16b709c4886960028251db2a
tvogelbacher <52699512+tvogelbacher@users.noreply.github.com>
2022-03-18 20:33:59 +0100

    Update article_nodes_relational_to_graph_complete.py

--- KnowledgeGraph/article_nodes_relational_to_graph_complete.py
+++ KnowledgeGraph/article_nodes_relational_to_graph_complete.py
@@ -1,17 +1,7 @@
-# Adding all articles to neo4j database
+# Adding all articles and Contains relations to neo4j database
 
 from neo4j import GraphDatabase
-import psycopg2
-
-HOST = ""
-PORT = ""
-DBNAME = ""
-USER = ""
-PASSWORD = ""
-
-HOST_GRAPH = ""
-USER_GRAPH = ""
-PASSWORD_GRAPH = ""
+from Database import Database
 
 class KGL_graph:
 
@@ -21,26 +11,47 @@ class KGL_graph:
     def close(self):
         self.driver.close()
 
-    def write(self, articleID, link, content, date, language):
+    def write_node(self, articleID, link, content, date, language):
         with self.driver.session() as session:
             session.run("CREATE (a:Article {ArticleID: '%d', link:'%s', content: '%s', date: '%s', language: '%s'})" % (articleID, link, content.replace("'", ""), date, language))
+            
+    def write_relation(self, contents, type):
+        with self.driver.session() as session:
+            for content in contents:
+                    if not [row["relation"] for row in session.run("MATCH (a:Article {link: '%s'}), (b:Company {name: '%s'}), p=(a)-[r:CONTAINS]->(b) RETURN p AS relation" % (content[0], content[1].replace("'", "")))]:
+                        session.run("MATCH (a:Article {link: '%s'}), (b:Company {name: '%s'}) CREATE (a)-[r:CONTAINS]->(b)" % (content[0], content[1].replace("'", "")))
+
+                    if not [row["relation"] for row in session.run("MATCH (a:Article {link: '%s'}), (b:%s WHERE '%s' IN b.synonyms), p=(a)-[r:CONTAINS]->(b) RETURN p AS relation" % (content[0], type, content[2]))]:
+                        session.run("MATCH (a:Article {link: '%s'}), (b:%s WHERE '%s' IN b.synonyms) CREATE (a)-[r:CONTAINS]->(b)" % (content[0], type, content[2]))
 
 
 if __name__ == "__main__":
-    graph = KGL_graph(HOST_GRAPH, USER_GRAPH, PASSWORD_GRAPH)
+    config = ConfigParser()
+    config.read("../config.ini")
+    
+    graph = KGL_graph(config["NEO4J"]["GRAPH_HOST"], config["NEO4J"]["GRAPH_USER"], config["NEO4J"]["GRAPH_PASSWORD"])
 
-    conn = psycopg2.connect(host=HOST, port=PORT, dbname=DBNAME, user=USER,
-                            password=PASSWORD)
-    cur = conn.cursor()
-    cur.execute("SELECT DISTINCT * FROM articles")
-    articles = cur.fetchall()
-    articleID = 0 # eigentlich Abfrage der derzeit höchsten ArticleID
+    articles = db.execute("SELECT DISTINCT * FROM articles")
+    articleID = 0
 
     for article in articles:
-        graph.write(articleID, article[0], article[1], article[2], article[3])
+        graph.write_node(articleID, article[0], article[1], article[2], article[3])
         articleID += 1
+        
+    
+    company_location_relation = db.execute("SELECT DISTINCT article, company, location FROM company_location_relations")
+    graph.write_relation(company_location_relation, "Location")
 
-    cur.close()
-    conn.close()
+    company_chemical_relation = db.execute("SELECT DISTINCT article, company, chemical FROM company_chemical_relations")
+    graph.write_relation(company_chemical_relation, "chemical_compound")
+
+    chemical_location_relation = db.execute("SELECT DISTINCT article, chemical, location FROM chemical_location_relations")
+    graph.write_relation(chemical_location_relation, "Location")
+
+    chemical_location_relation = db.execute("SELECT DISTINCT article, location, chemical FROM chemical_location_relations")
+    graph.write_relation(chemical_location_relation, "chemical_compound")
+    
+
+    db.close()
 
     graph.close()
